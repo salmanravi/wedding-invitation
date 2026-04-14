6f964fc7644f8a4994397074c725fa5d2b4265de
salmanravi <salmanravi0@gmail.com>
2026-04-14 20:32:12 +0700

    Update data.js

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -2,7 +2,7 @@ export const data = {
     bride: {
         L: {
             id: 1,
-            name: 'Lorem Ipsum',
+            name: 'Egi Tohari',
             child: 'Putra ke lorem',
             father: 'Lorem',
             mother: 'Ipsum',
@@ -10,7 +10,7 @@ export const data = {
         },
         P: {
             id: 2,
-            name: 'Ipsum Lorem',
+            name: 'Siti Maemunah',
             child: 'Putri ke lorem',
             father: 'Lorem',
             mother: 'Ipsum',
