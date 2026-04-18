502287913c20ed5acdb41d2092b6355ff1d3b739
salmanravi <salmanravi0@gmail.com>
2026-04-18 16:09:44 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,17 +1,15 @@
-import {home} from "./js/home.js";
-import {bride} from "./js/bride.js";
-import {time} from "./js/time.js";
-import {galeri} from "./js/galeri.js";
-import {wishas} from "./js/wishas.js";
-import {navbar} from "./js/navbar.js";
-import {welcome} from "./js/welcome.js";
+import { home } from "./js/home.js";
+import { bride } from "./js/bride.js";
+import { time } from "./js/time.js";
+import { galeri } from "./js/galeri.js";
+import { wishas } from "./js/wishas.js";
+import { navbar } from "./js/navbar.js";
+import { welcome } from "./js/welcome.js";
 
-// load content
-// Ambil parameter dari URL
+// Ambil parameter URL
 const urlParams = new URLSearchParams(window.location.search);
 const guestSlug = urlParams.get("to");
 
-// Ubah slug → nama normal
 function slugToName(slug) {
   if (!slug) return "Tamu Undangan";
 
@@ -23,46 +21,19 @@ function slugToName(slug) {
 const guestName = slugToName(guestSlug);
 
 document.addEventListener("DOMContentLoaded", () => {
+
   document.querySelectorAll(".guest-name").forEach(el => {
     el.textContent = guestName;
   });
-});
 
-const supabase = window.supabase.createClient(
-  "https://vccqbcooezemimdwemui.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZjY3FiY29vZXplbWltZHdlbXVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY0OTk0MTUsImV4cCI6MjA5MjA3NTQxNX0.GW_jwb98pY5f2yTquL6bfd2ZbZ1gveODjg5K9pbQ0oA"
-);
+  AOS.init();
 
-const form = document.querySelector("form");
-const list = document.querySelector("ul[aria-label='list comentar']");
+  welcome();
+  navbar();
+  home();
+  bride();
+  time();
+  galeri();
+  wishas(); // 🔥 hanya ini yang handle ucapan
 
-// === INSERT DATA ===
-form.addEventListener("submit", async (e) => {
-  e.preventDefault();
-
-  const name = document.getElementById("name").value;
-  const status = document.getElementById("status").value;
-  const message = document.getElementById("message").value;
-
-  const { error } = await supabase.from("wishes").insert([
-    { name, status, message }
-  ]);
-
-  if (error) {
-    alert("Gagal kirim");
-  } else {
-    form.reset();
-  }
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-    AOS.init();
-
-    welcome();
-    navbar();
-    home();
-    bride()
-    time();
-    galeri();
-    wishas();
 });
