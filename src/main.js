7bfc20b7f813867236cc6d63c6a32efb8cb5be86
salmanravi <salmanravi0@gmail.com>
2026-04-18 14:46:21 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,6 +7,21 @@ import {navbar} from "./js/navbar.js";
 import {welcome} from "./js/welcome.js";
 
 // load content
+// Ambil parameter dari URL
+const urlParams = new URLSearchParams(window.location.search);
+const guestSlug = urlParams.get("to");
+
+// Ubah slug → nama normal
+function slugToName(slug) {
+  if (!slug) return "Tamu Undangan";
+
+  return slug
+    .replace(/-/g, " ")
+    .replace(/\b\w/g, char => char.toUpperCase());
+}
+
+const guestName = slugToName(guestSlug);
+
 document.addEventListener('DOMContentLoaded', () => {
     AOS.init();
 
